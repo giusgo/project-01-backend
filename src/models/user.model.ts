2ce744e922c0feff15524721303c02c80226fdfd
giusgo <giuseppeg@uninorte.edu.co>
2025-05-08 22:50:19 -0500

    add: user permisions

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,8 +1,17 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+export interface IUserPermissions {
+  createBooks: boolean;
+  modifyUsers: boolean;
+  modifyBooks: boolean;
+  deleteUsers: boolean;
+  deleteBooks: boolean;
+}
+
 export interface IUser extends Document {
   email: string;
   password: string;
+  permissions: IUserPermissions;
   isDeleted: boolean;
   createdAt: Date;
   updatedAt: Date;
@@ -20,6 +29,28 @@ const UserSchema: Schema = new Schema(
     password: {
       type: String,
       required: true,
+    },
+    permissions: {
+      createBooks: {
+        type: Boolean,
+        default: false,
+      },
+      modifyUsers: {
+        type: Boolean,
+        default: false,
+      },
+      modifyBooks: {
+        type: Boolean,
+        default: false,
+      },
+      deleteUsers: {
+        type: Boolean,
+        default: false,
+      },
+      deleteBooks: {
+        type: Boolean,
+        default: false,
+      },
     },
     isDeleted: {
       type: Boolean,
